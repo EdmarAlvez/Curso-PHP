0f03d2024ba1884825a40ca87bb5227cd409cdec
Edmar Alves <47756547+EdmarAlvez@users.noreply.github.com>
2019-08-20 16:20:25 -0300

    Curso PHP

---
+++ funcoes_strings.php
@@ -0,0 +1,12 @@
+<?php
+
+//strtolower todas minusculas
+//srttoupper todas maiusculas
+//ucfirst Primera letra Maiuscula
+
+$texto = "curso completo de php";
+
+//echo strtolower($texto);
+//echo strtoupper($texto);
+echo ucfirst($texto);
+?>
