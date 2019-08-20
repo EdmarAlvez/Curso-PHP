cc439ccfb70074c25954d29fd7f9d7d26cfe861f
Edmar Alves <47756547+EdmarAlvez@users.noreply.github.com>
2019-08-20 16:53:00 -0300

    CURSO PHP
    FUNCOES STRINGS

--- funcoes_strings.php
+++ funcoes_strings.php
@@ -3,10 +3,41 @@
 //strtolower todas minusculas
 //srttoupper todas maiusculas
 //ucfirst Primera letra Maiuscula
+//strlen Ver o tamanho da string
 
-$texto = "curso completo de php";
+
+//$texto = "curso completo de php";
 
 //echo strtolower($texto);
 //echo strtoupper($texto);
-echo ucfirst($texto);
+//echo ucfirst($texto);
+//echo strlen($texto);
+//str_replace
+//substr
+
+/*$cpf = isset($_POST['cpf']) ? ($_POST['cpf']) :'';
+$total_string_cpf = strlen($cpf); 
+
+if($total_string_cpf !=11 && $cpf != ''){
+	echo "CPF invalido<br>";
+}if($total_string_cpf == 11 && $cpf != ''){
+	echo "Cadastrado<br>";
+}*/
+
+/*$texto = "12.40.54654.44";
+echo str_replace('.', ',', $texto);
+echo "<br><br>";*/
+
+$texto = "Edmar Alves da Silva filho";
+$noticia = substr($texto, 0,10);
+echo $noticia." ....";
+
+
 ?>
+
+<form method="post" action="">
+	<label>CPF
+	<input type="texto" name="cpf">
+	</label>
+	<input type="submit" value="cadastrar"name="">
+</form> 
